0b657b0187332420fa2900fa6ccc7457a2bcc9d4
Elodie Riou <elodie.riou@hotmail.com>
2023-06-29 16:23:30 +0200

    FEATURE: 3. Initialize the main App component

--- cinema/src/App.js
+++ cinema/src/App.js
@@ -1,8 +1,26 @@
 import './App.css';
+import { useState, useEffect } from "react";
+import axios from 'axios';
 
 function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userUsername, setUserUsername] = useState('');
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem('accessToken');
+    axios.post('http://localhost:8000/api/auth/', {}, { headers: { 'authorization': `Bearer ${accessToken}`} })
+        .then((response) => {
+          setIsLoggedIn(true);
+          setUserUsername(response.data.username);
+        })
+        .catch((error) => console.log(error))
+  }, []);
+
   return (
     <div className="App">
+      { isLoggedIn && <p>Dashboard</p> }
+      { !isLoggedIn && <p>Authentication</p> }
     </div>
   );
 }
