746d4b999bb06172a368092e3234e45e83770821
Elodie Riou <elodie.riou@hotmail.com>
2023-07-03 15:08:02 +0200

    FEATURE: add dashbord routing

--- cinema/src/routes/dashboard/Dashboard.js
+++ cinema/src/routes/dashboard/Dashboard.js
@@ -3,14 +3,23 @@ import Header from '../../components/navigation/Header';
 import Sidebar from '../../components/navigation/SideBar';
 import React from 'react';
 import PropTypes from 'prop-types';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
 
 export default function Dashboard (props) {
     const { userUsername, setIsLoggedIn } = props;
     return (
-        <div>
+        <Router>
+            <Routes>
+                <Route path={"/"} />
+                <Route path={"/favorites"} />
+                <Route path={"/watchlater"} />
+                <Route path={"*"} element={<Navigate to={"/"}/>}/>
+            </Routes>
+
             <Header userUsername={userUsername} setIsLoggedIn={setIsLoggedIn}/>
             <Sidebar />
-        </div>
+        </Router>
     )
 }
 
